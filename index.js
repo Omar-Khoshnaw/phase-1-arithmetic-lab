7dab1c4f5986ef4bb7dc438e974af49f4dd6c44b
Omar Sardar <omar.khoshnaw@gmail.com>
2024-02-10 23:04:44 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
+let num1= 2
+let num2= 31
+let num3= 19
+let num4= 20
+let multiply = num1 * num2
+console.log(multiply)
 // Write your code here
+
+let random= Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+let  mod = 25 % 7;
+
+
+let max= Math.max(num1, num3, num4)
+console.log(max)
